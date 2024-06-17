7eb325f5509a6028499e5b0b249591b588196422
Vega Nicholas S. <veganicholas123@gmail.com>
2024-06-17 11:03:03 +0700

    fix: cs

--- src/endstone_server_maintenance/event_listener.py
+++ src/endstone_server_maintenance/event_listener.py
@@ -2,22 +2,29 @@ from datetime import datetime
 
 from endstone.event import event_handler, EventPriority, PlayerLoginEvent
 
+
 class EventListener:
     def __init__(self, plugin) -> None:
         from endstone_server_maintenance.server_maintenance import ServerMaintenance
+
         if isinstance(plugin, ServerMaintenance):
-             self._plugin = plugin
-    
+            self._plugin = plugin
 
     @event_handler(priority=EventPriority.HIGHEST)
     def on_player_login(self, event: PlayerLoginEvent) -> None:
-            is_maintenance = self._plugin.is_maintenance()
-            message = self._plugin.MESSAGE
-            start_date_time = datetime.fromtimestamp(self._plugin._maintenance["start_date_time"])
-            end_date_time = datetime.fromtimestamp(self._plugin._maintenance["end_date_time"])
+        is_maintenance = self._plugin.is_maintenance()
+        message = self._plugin.MESSAGE
+        start_date_time = datetime.fromtimestamp(
+            self._plugin._maintenance["start_date_time"]
+        )
+        end_date_time = datetime.fromtimestamp(
+            self._plugin._maintenance["end_date_time"]
+        )
 
-            is_allowed = self._plugin.is_player_allowed(event.player.name)
-            if is_maintenance == True and not is_allowed:
-                kick_message = message.format(start_date_time=start_date_time, end_date_time=end_date_time)
-                event.kick_message = kick_message
-                event.cancelled = True
+        is_allowed = self._plugin.is_player_allowed(event.player.name)
+        if is_maintenance == True and not is_allowed:
+            kick_message = message.format(
+                start_date_time=start_date_time, end_date_time=end_date_time
+            )
+            event.kick_message = kick_message
+            event.cancelled = True
--- src/endstone_server_maintenance/server_maintenance.py
+++ src/endstone_server_maintenance/server_maintenance.py
@@ -5,6 +5,7 @@ from endstone._internal.endstone_python import Command, CommandSender
 from endstone._internal.plugin_loader import PermissionDefault
 from endstone.plugin import Plugin
 
+
 class ServerMaintenance(Plugin):
     name = "ServerMaintenance"
     version = "0.1.0"
@@ -14,18 +15,18 @@ class ServerMaintenance(Plugin):
     website = "https://github.com/nicholass003/endstone-server-maintenance"
     load = "POSTWORLD"
 
-    MESSAGE = '''Server Maintenance Notice
+    MESSAGE = """Server Maintenance Notice
     Server is currently undergoing maintenance.
     Maintenance Period: {start_date_time} to {end_date_time}
-    Thank you for your patience and support.'''
+    Thank you for your patience and support."""
 
     commands = {
         "maintenance": {
             "description": "Allow users to use all commands provided by this plugin.",
             "usages": [
                 "/maintenance ()[enum: EnumType] [text: string]",
-                "/maintenance (add|on|off|remove)[enum: EnumType] [text: string]"
-                ],
+                "/maintenance (add|on|off|remove)[enum: EnumType] [text: string]",
+            ],
             "aliases": ["mtc"],
             "permissions": ["server_maintenance.command"],
         },
@@ -49,26 +50,31 @@ class ServerMaintenance(Plugin):
 
     def on_load(self) -> None:
         self._maintenance = {
-            "is_maintenance" : False,
-            "start_date_time" : 0,
-            "end_date_time" : 0,
+            "is_maintenance": False,
+            "start_date_time": 0,
+            "end_date_time": 0,
         }
 
-        self._maintenance_players = {
-            "players" : []
-        }
+        self._maintenance_players = {"players": []}
 
     def on_enable(self) -> None:
         from endstone_server_maintenance.event_listener import EventListener
+
         self._listener = EventListener(self)
         self.register_events(self._listener)
-        self.server.scheduler.run_task_timer(self, self.maintenance_task, delay=0, period=10)
+        self.server.scheduler.run_task_timer(
+            self, self.maintenance_task, delay=0, period=10
+        )
 
-    def on_command(self, sender: CommandSender, command: Command, args: list[str]) -> bool:
+    def on_command(
+        self, sender: CommandSender, command: Command, args: list[str]
+    ) -> bool:
         if not sender.has_permission("server_maintenance.command"):
-            sender.send_error_message(f"{ColorFormat.RED}You don't have permission to use this command.")
+            sender.send_error_message(
+                f"{ColorFormat.RED}You don't have permission to use this command."
+            )
             return True
-        
+
         match command.name:
             case "maintenance":
                 if len(args) > 0:
@@ -77,55 +83,101 @@ class ServerMaintenance(Plugin):
                         case "add":
                             if len(args) > 1:
                                 if self.add_maintenance_player(args[1].lower()) == True:
-                                    sender.send_message(f"{ColorFormat.GREEN}Successfully added {args[1]} to maintenance players")
+                                    sender.send_message(
+                                        f"{ColorFormat.GREEN}Successfully added {args[1]} to maintenance players"
+                                    )
                                 else:
-                                    sender.send_error_message(f"{ColorFormat.RED}Player with gamertag {args[1]} is exists!")
+                                    sender.send_error_message(
+                                        f"{ColorFormat.RED}Player with gamertag {args[1]} is exists!"
+                                    )
                             else:
-                                sender.send_error_message(f"{ColorFormat.RED}Please input player name.")
+                                sender.send_error_message(
+                                    f"{ColorFormat.RED}Please input player name."
+                                )
                         case "on":
-                            if(self.handle_activation(sender=sender, action=self.ACTION_ENABLE, value=True) == True):
+                            if (
+                                self.handle_activation(
+                                    sender=sender, action=self.ACTION_ENABLE, value=True
+                                )
+                                == True
+                            ):
                                 current_time = int(datetime.now().timestamp())
                                 if len(args) > 1:
                                     end_value = self.convert_to_integer(args[1])
-                                    if(end_value == None):
-                                        sender.send_error_message(f"{ColorFormat.RED}Please input numeric only.")
+                                    if end_value == None:
+                                        sender.send_error_message(
+                                            f"{ColorFormat.RED}Please input numeric only."
+                                        )
                                         return True
-                                    self._maintenance["end_date_time"] = current_time + int(timedelta(hours=end_value).total_seconds())
-                                    sender.send_message(f"{ColorFormat.RED}Setting Maintenance for {end_value} hour ahead.")
+                                    self._maintenance["end_date_time"] = (
+                                        current_time
+                                        + int(
+                                            timedelta(hours=end_value).total_seconds()
+                                        )
+                                    )
+                                    sender.send_message(
+                                        f"{ColorFormat.RED}Setting Maintenance for {end_value} hour ahead."
+                                    )
                                 else:
-                                    self._maintenance["end_date_time"] = current_time + int(timedelta(hours=1).total_seconds())
-                                    sender.send_message(f"{ColorFormat.RED}Setting default Maintenance for 1 hour ahead.")
-                                    
+                                    self._maintenance["end_date_time"] = (
+                                        current_time
+                                        + int(timedelta(hours=1).total_seconds())
+                                    )
+                                    sender.send_message(
+                                        f"{ColorFormat.RED}Setting default Maintenance for 1 hour ahead."
+                                    )
+
                                 self._maintenance["start_date_time"] = current_time
                         case "off":
-                            self.handle_activation(sender=sender, action=self.ACTION_DISABLE, value=False)
+                            self.handle_activation(
+                                sender=sender, action=self.ACTION_DISABLE, value=False
+                            )
                         case "remove":
                             if len(args) > 1:
-                                if self.remove_maintenance_player(args[1].lower()) == True:
-                                    sender.send_message(f"{ColorFormat.GREEN}Successfully removed {args[1]} to maintenance players")
+                                if (
+                                    self.remove_maintenance_player(args[1].lower())
+                                    == True
+                                ):
+                                    sender.send_message(
+                                        f"{ColorFormat.GREEN}Successfully removed {args[1]} to maintenance players"
+                                    )
                                 else:
-                                    sender.send_error_message(f"{ColorFormat.RED}Player with gamertag {args[1]} is not exists!")
+                                    sender.send_error_message(
+                                        f"{ColorFormat.RED}Player with gamertag {args[1]} is not exists!"
+                                    )
                             else:
-                                sender.send_error_message(f"{ColorFormat.RED}Please input player name.")
+                                sender.send_error_message(
+                                    f"{ColorFormat.RED}Please input player name."
+                                )
                         case _:
-                            sender.send_error_message(f"{ColorFormat.RED}There are no maintenance command with name {args[0]}.")
+                            sender.send_error_message(
+                                f"{ColorFormat.RED}There are no maintenance command with name {args[0]}."
+                            )
                 else:
-                    sender.send_error_message(f"{ColorFormat.RED}Usage: /maintenance [args].")
+                    sender.send_error_message(
+                        f"{ColorFormat.RED}Usage: /maintenance [args]."
+                    )
                     return True
         return True
-    
-    def handle_activation(self, sender: CommandSender, action: str, value: bool) -> bool:
+
+    def handle_activation(
+        self, sender: CommandSender, action: str, value: bool
+    ) -> bool:
         current_state = self.is_maintenance
 
         if current_state == value:
-            sender.send_error_message(f"{ColorFormat.RED}Server Maintenance was previously {action}")
+            sender.send_error_message(
+                f"{ColorFormat.RED}Server Maintenance was previously {action}"
+            )
             return False
         else:
             self._maintenance["is_maintenance"] = value
             if value == False:
                 self._maintenance["start_date_time"] = 0
                 self._maintenance["end_date_time"] = 0
-            sender.send_message(f"{ColorFormat.GREEN}Server Maintenance has been set to {action}.")
+            sender.send_message(
+                f"{ColorFormat.GREEN}Server Maintenance has been set to {action}."
+            )
         return True
 
     def convert_to_integer(self, string_value: str) -> int | None:
@@ -134,7 +186,7 @@ class ServerMaintenance(Plugin):
             return integer_value
         except ValueError as err:
             return None
-        
+
     def maintenance_task(self):
         if int(datetime.now().timestamp()) >= self._maintenance["end_date_time"]:
             if self.is_maintenance == False:
@@ -143,16 +195,16 @@ class ServerMaintenance(Plugin):
 
     def is_maintenance(self) -> bool:
         return self._maintenance["is_maintenance"]
-    
+
     def get_maintenance(self) -> object:
         return self._maintenance
-    
+
     def set_maintenance(self, object: object) -> None:
         self._maintenance = object
 
     def get_maintenance_players(self) -> object:
         return self._maintenance_players
-    
+
     def is_player_allowed(self, name: str) -> bool:
         while name in self._maintenance_players:
             return True
@@ -163,7 +215,7 @@ class ServerMaintenance(Plugin):
             self._maintenance_players.append(name)
             return True
         return False
-    
+
     def remove_maintenance_player(self, name: str) -> bool:
         while name in self._maintenance_players:
             self._maintenance_players.remove(name)
